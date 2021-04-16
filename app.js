b9169f77f0c0aa80fd7b7aea121b689a308a183d
Michał Marchewa <michalmarchewa@gmail.com>
2021-04-16 16:12:59 +0200

    layout.pug- maxin added

--- app.js
+++ app.js
@@ -22,6 +22,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+  res.locals.path = req.path
+  next()
+})
 app.use('/', indexRouter);
 app.use('/news', newsRouter);
 app.use('/quiz', quizRouter);
