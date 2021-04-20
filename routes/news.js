cdf72887e11ac1c01cf4b535243eafcbc9fb37c7
MarcheM <43741051+MarcheM@users.noreply.github.com>
2021-04-20 12:56:00 +0200

    Update news.js

--- routes/news.js
+++ routes/news.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
   const findNews = News
     .find({title: new RegExp(search.trim(), 'i')})
     .sort({ created: -1 });
-
+console.log(findNews)
   findNews.exec((err, data) => {
     res.render('news', { title: 'News', data, search });
   })
